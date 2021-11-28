ff1f671ee89f54437224222fa753e3fd7909f356
Raul Rodriguez <raul0882@gmail.com>
2021-11-27 20:52:28 -0500

    Added comic book repository

--- ComicBookGalleryMVC/Controllers/ComicBookController.cs
+++ ComicBookGalleryMVC/Controllers/ComicBookController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using ComicBookGalleryMVC.Models;
 
 namespace ComicBookGalleryMVC.Controllers
 {
@@ -7,22 +8,25 @@ namespace ComicBookGalleryMVC.Controllers
     {
         public ActionResult Detail()
         {
-            var comicBook = new Models.ComicBook()
+
+            var comicBook = new ComicBook()
             {
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 700,
                 DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artist = new Models.Artist[]
-                {
-                    new Models.Artist(){ Name="Dan Slot", Role = "Script"},
-                    new Models.Artist(){ Name="Humberto Ramos", Role = "Pencils"},
-                    new Models.Artist(){ Name="Victor Olazaba", Role = "Inks"},
-                    new Models.Artist(){ Name="Edgar Delgado", Role = "Colors"},
-                    new Models.Artist(){ Name="Chris Eliopoulos", Role = "Letters"},
-                }
-            };
 
+                Artist = new Models.Artist[]
+               {
+                    new Artist(){ Name="Dan Slot", Role = "Script"},
+                    new Artist(){ Name="Humberto Ramos", Role = "Pencils"},
+                    new Artist(){ Name="Victor Olazaba", Role = "Inks"},
+                    new Artist(){ Name="Edgar Delgado", Role = "Colors"},
+                    new Artist(){ Name="Chris Eliopoulos", Role = "Letters"},
+               }
+            };
             
+
+
 
 
 
